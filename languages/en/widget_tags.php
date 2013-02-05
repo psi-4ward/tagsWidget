30ef30717c00aa22f4bef38233a8c3612b50c1df
Christoph Wiechert <wio@psitrax.de>
2013-02-05 19:48:27 +0100

    Added english translation: Many Credits to Jozef Dvorsky

---
+++ languages/en/widget_tags.php
@@ -0,0 +1,18 @@
+<?php if(!defined('TL_ROOT')) {die('You cannot access this file directly!');}
+
+/**
+ * Tags Widget for Contao
+ *
+ * @author Christoph Wiechert <wio@psitrax.de>
+ * @copyright 4ward.media GbR <http://www.4wardmedia.de>
+ * @package tagsWidget
+ * @filesource
+ * @licence LGPL
+ */
+
+
+$GLOBALS['TL_LANG']['widget_tags']['inputPlaceholder']		= '+ add tag';
+$GLOBALS['TL_LANG']['widget_tags']['limitReached']			= 'This tag is too long, or max. number of tags is exceeded';
+$GLOBALS['TL_LANG']['widget_tags']['toShort']				= 'Tag is too short';
+$GLOBALS['TL_LANG']['widget_tags']['del']					= 'Delete tag';
+
